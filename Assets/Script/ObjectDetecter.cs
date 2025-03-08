03b6d5913f382e92a1fe511ead7b97e47e1b6fb6
DESKTOP-19P5MFT\newpr <100wja100@gmail.com>
2025-03-08 10:59:21 +0900

    Feat : 7강_타워가 아닌 부분을 클릭했을 때 타워 정보 비활성화

--- Assets/Script/ObjectDetecter.cs
+++ Assets/Script/ObjectDetecter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ObjectDetecter : MonoBehaviour
 {
@@ -7,7 +8,7 @@ public class ObjectDetecter : MonoBehaviour
     private Camera mainCamera;
     private Ray ray;
     private RaycastHit hit;
-
+    private Transform hitTransform = null; //마우스 픽킹으로 선택한 오브젝트 임시 저장
     private void Awake()
     {
         mainCamera = Camera.main;
@@ -15,6 +16,10 @@ public class ObjectDetecter : MonoBehaviour
 
     private void Update()
     {
+        if (EventSystem.current.IsPointerOverGameObject() == true) //마우스가 UI에 머물러 있을 때는 아래 코드가 실행되지 않도록 함
+        {
+            return;
+        }
         if (Input.GetMouseButtonDown(0)) //마우스 왼쪽 버튼을 눌렀을 때
         {
             // ray.origin : 광선의 시작 위치 ( = 카메라 위치 )
@@ -26,6 +31,8 @@ public class ObjectDetecter : MonoBehaviour
                 // ray이 감지할 수 있는 최대 거리. 만약 멀리 있는 오브젝트는 클릭해도 안먹게 하고싶다면
                 // 길이를 조정할 수 있다.
             {
+                hitTransform = hit.transform;
+                
                     // 광선에 부딪힌 오브젝트의 태그가 "Tile"이면
                 if (hit.transform.CompareTag("Tile"))
                 {
@@ -37,6 +44,18 @@ public class ObjectDetecter : MonoBehaviour
                     towerDataViewer.OnPanel(hit.transform);
                 }
             }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // 마우스를 눌렀을 때 선택한 오브젝트가 없거나 선택한 오브젝트가 타워가 아니면 타워 정보가 사라진다.
+            if (hitTransform == null || hitTransform.CompareTag("Tower") == false)
+            {
+                // 타워 정보 패널을 비활성화 한다
+                towerDataViewer.OffPanel();
+            }
+
+            // 선택된 오브젝트 초기화
+            hitTransform = null;
         }
     }
 }
